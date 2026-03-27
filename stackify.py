4a2f1b4fb9010efe99697d2b94bab279d60d8148
kweezo <jakobtadejv@gmail.com>
2026-03-27 20:43:37 +0100

    [UPD] Switched color channels to align with BGRA SDL expects

--- stackify.py
+++ stackify.py
@@ -7,7 +7,7 @@ if len(sys.argv) != 3:
     print("Input 2 args, input and output")
     sys.exit(1)
 
-dat = np.array(Image.open(sys.argv[1]))
+dat = np.array(Image.open(sys.argv[1]).convert("RGBA"))
 
 out = open(sys.argv[2], "wb")
 out.write(bytes(f"""
@@ -18,11 +18,10 @@ uint8_t texDat[] = {'{'}
 
 for row in dat:
     for pixel in row:
-        for channel in range(min(len(pixel), 4)):
+        for channel in range(2, -1, -1):
             out.write(bytes(str(pixel[channel]) + ", ", encoding="ascii"))
+        out.write(bytes(str(pixel[3]) + ", ", encoding="ascii"))
 
-        for i in range(4 - len(pixel)):
-            out.write(bytes(str(255) + ", ", encoding="ascii"))
 out.seek(-2, os.SEEK_CUR)
 out.write(bytes("};", encoding="ascii"))
 out.close()
